814214a23e0400104b1525f055ed6e95eccb084d
Justin Bishop <jubishop@gmail.com>
2021-04-29 22:31:11 -0700

    optionize thing

--- lib/static/long_cache.rb
+++ lib/static/long_cache.rb
@@ -1,21 +1,32 @@
 require 'rack'
 
 module Tony
-  class LongCache
-    def initialize(app)
-      @app = app
-      @file_server = Rack::File.new(::File.join(Dir.pwd, PUBLIC_FOLDER))
-    end
+  module Static
+    class LongCache
+      def initialize(app, **options)
+        @app = app
+        @options = {
+          public_folder: 'public',
+          headers: {
+            'Cache-Control': 'public, max-age=31536000, immutable'
+          }
+        }.merge(options)
+        @file_server = Rack::File.new(::File.join(Dir.pwd,
+                                                  @options[:public_folder]))
+      end
 
-    def call(env)
-      req = Rack::Request.new(env)
-      return @app.call(env) unless req.get?
+      def call(env)
+        req = Rack::Request.new(env)
+        return @app.call(env) unless req.get?
 
-      status, headers, body = @file_server.call(env)
-      return @app.call(env) if status == 404
+        status, headers, body = @file_server.call(env)
+        return @app.call(env) if status == 404
 
-      headers['Cache-Control'] = 'public, max-age=31536000, immutable'
-      return [status, headers, body]
+        @options[:headers].each { |name, value|
+          headers[name] = value
+        }
+        return [status, headers, body]
+      end
     end
   end
 end
