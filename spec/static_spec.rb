46d2cd4eaaac7dd644a5ea339898fd8046e107e5
Justin Bishop <jubishop@gmail.com>
2021-05-01 16:33:24 -0700

    fully tested Tony::Static

--- spec/static_spec.rb
+++ spec/static_spec.rb
@@ -1,10 +1,42 @@
 require_relative '../lib/static'
 
 RSpec.describe(Tony::Static, type: :rack_test) {
-  let(:app) { Tony::Static.new }
+  context('file not found') {
+    it('returns 404 if no more middleware left') {
+      def app
+        Tony::Static.new
+      end
 
-  it('returns 404 when file not found') {
-    get '/does_not_exist.css'
-    expect(last_response.status).to(eq(404))
+      get '/does_not_exist.css'
+      expect(last_response.status).to(eq(404))
+    }
+
+    it('calls next middleware') {
+      def app
+        next_app = lambda { |_|
+          [418, { "Content-Type": 'text/plain' }, ["I'm a teapot'"]]
+        }
+        Tony::Static.new(next_app)
+      end
+
+      get '/does_not_exist.css'
+      expect(last_response.status).to(eq(418))
+    }
+  }
+
+  context('file found') {
+    let(:app) { Tony::Static.new(public_folder: 'spec/assets') }
+
+    it('returns top level file') {
+      get '/test.css'
+      expect(last_response.status).to(eq(200))
+      expect(last_response.body).to(eq("body { color: black; }\n"))
+    }
+
+    it('returns file nested in folder') {
+      get '/nested/test.css'
+      expect(last_response.status).to(eq(200))
+      expect(last_response.body).to(eq("body { color: white; }\n"))
+    }
   }
 }
