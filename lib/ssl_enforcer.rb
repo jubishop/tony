29d7209bb149ad454d987b83e9e5979e83258850
Justin Bishop <jubishop@gmail.com>
2021-05-02 13:15:32 -0700

    ssl enforcer

---
+++ lib/ssl_enforcer.rb
@@ -0,0 +1,27 @@
+module Tony
+  class SSLEnforcer
+    def initialize(app, **options)
+      @app = app
+      @options = options
+    end
+
+    def call(env)
+      if req.scheme == 'http' || req.env['HTTP_X_FORWARDED_SSL'] == 'off'
+        location = "https://#{req.host_with_port}#{req.fullpath}"
+        body = <<~HTML
+          <html>
+            <body>
+              You are being <a href="#{location}">redirected</a>.
+            </body>
+          </html>
+        HTML
+        return [
+          301,
+          { "Content-Type": 'text/html', Location: location },
+          [body]
+        ]
+      end
+      return @app.call(env)
+    end
+  end
+end
