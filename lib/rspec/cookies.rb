2d55b1b8a390737a4f0409d6c9e586ddd05aeaa8
Justin Bishop <jubishop@gmail.com>
2021-05-01 22:52:27 -0700

    correcting rspec helper

--- lib/rspec/cookies.rb
+++ lib/rspec/cookies.rb
@@ -4,11 +4,13 @@ module Tony
   module RSpec
     module Cookies
       def set_cookie(name, value)
-        rack_mock_session.cookie_jar[name] = Tony::Utils::Crypt.en(value)
+        crypt = Tony::Utils::Crypt.new(cookie_secret)
+        rack_mock_session.cookie_jar[name] = crypt.en(value)
       end
 
       def get_cookie(name)
-        return Tony::Utils::Crypt.de(rack_mock_session.cookie_jar[name])
+        crypt = Tony::Utils::Crypt.new(cookie_secret)
+        return crypt.de(rack_mock_session.cookie_jar[name])
       end
     end
   end
