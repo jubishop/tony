e6b73d53db19d54752cdaf12565f0bdfb301a7bb
Justin Bishop <jubishop@gmail.com>
2021-05-03 15:25:07 -0700

    move to lambda's, add more tests

--- lib/app.rb
+++ lib/app.rb
@@ -15,27 +15,15 @@ module Tony
 
         puts match if match.is_a?(MatchData) # TODO: named_captures
         resp = Response.new
-        route.block.run(req, resp)
+        route.block.call(req, resp)
         return resp.finish
       }
 
       return @app.call(env)
     end
 
-    def get(path, &block)
+    def get(path, block)
       @routes['GET'].push(Route.new(path, block))
-    end
-
-    def search(req, routes, status)
-      routes.each { |route|
-        next unless (match = route.match?(req.path))
-
-        puts match if match.is_a?(MatchData)
-        resp = Response.new(status)
-        route.block.run(req, resp)
-        return resp.finish
-      }
-      return false
     end
 
     class Route
@@ -53,10 +41,11 @@ module Tony
     end
 
     class Response < Rack::Response
+      undef :body= # use write()
+
       def finish
         headers.transform_keys!(&:to_s)
-        self['Content-Length'] ||= body.bytesize
-        self['Content-Type'] ||= 'text/html;charset=utf-8'
+        headers['Content-Type'] ||= 'text/html;charset=utf-8'
         return super
       end
     end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -3,22 +3,38 @@ RSpec.describe(Tony::App, type: :rack_test) {
 
   it('returns default 200 status on get') {
     @app = Tony::App.new
-    app.get('/') { |_, resp|
-      resp.body = 'Hello World'
-    }
+    app.get('/', ->(_, resp) {})
     get '/'
 
     expect(last_response.status).to(be(200))
   }
 
+  it('deals with returning from a block') {
+    @app = Tony::App.new
+    app.get('/', lambda { |_, _|
+      return
+    })
+    get '/'
+
+    expect(last_response.status).to(be(200))
+  }
+  it('does not allow writing to body') {
+    @app = Tony::App.new
+    app.get('/', lambda { |_, resp|
+      resp.body = 'Hello World'
+    })
+
+    expect { get('/') }.to(raise_error(NoMethodError))
+  }
+
   context('simple getter') {
     before(:each) {
       @app = Tony::App.new
-      app.get('/') { |_, resp|
-        resp.body = 'Hello World'
+      app.get('/', lambda { |_, resp|
+        resp.write('Hello World')
         resp.status = 418
         resp.headers[:CUSTOM] = 'HEADER'
-      }
+      })
       get '/'
     }
 
@@ -35,7 +51,7 @@ RSpec.describe(Tony::App, type: :rack_test) {
     }
 
     it('sets content-length') {
-      expect(last_response.headers['Content-Length']).to(eq(11))
+      expect(last_response.headers['Content-Length']).to(eq('11'))
     }
 
     it('sets content type') {
